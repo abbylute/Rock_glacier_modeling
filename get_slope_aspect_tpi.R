7e0a2cf58f9827f730133f699de6c3c0db71966e
abbylute <aby.lute@gmail.com>
2020-11-06 16:22:59 -0800

    update to run headwall

--- get_slope_aspect_tpi.R
+++ get_slope_aspect_tpi.R
@@ -1,9 +1,15 @@
 # Calculate terrain aspect and slope
 
 library(raster)
-outdir <- '/Volumes/WDPassport/Rock_glacier_research/WUS/Data/Topography/'
-dem <- raster('/Volumes/WDPassport/DATA/DEM/NED/210m/WUS_NED_210m.tif')
-source('/Volumes/WDPassport/Rock_glacier_research/WUS/Code/calc_headwall.R')
+library(parallel)
+clnum <- detectCores()-2
+
+mdir <- '/Volumes/WDPassport/'
+mdir <- '/home/abby/'
+outdir <- paste0(mdir,'Rock_glacier_research/WUS/Data/Topography/')
+#dem <- raster('/Volumes/WDPassport/DATA/DEM/NED/210m/WUS_NED_210m.tif')
+dem <- raster(paste0(mdir,'DATA/Mapping/WUS_NED_210m.tif')
+source(paste0(mdir,'Rock_glacier_research/WUS/Code/calc_headwall.R'))
 
 # create slope raster:
 terchar = terrain(dem, opt=c('slope','aspect','TPI'), unit='degrees', neighbors=8)
@@ -23,29 +29,42 @@ writeRaster(aspect,paste0(outdir,'aspect_210m_WUS.tif'))
 # calculate headwall metric
 buf = 400 #radius in meters
 slope_thres = 30 # threshold slope in degrees
-i = 1:5000000;#length(values(dem))
-hw <- calc_headwall(iter=i,demsmall2=dem,slope=slope,buf=buf,slope_thres=slope_thres)
 
-
-# old code
-use <- which(!is.na(values(demsmall2)))
+use <- which(!is.na(values(dem)))
 use <- matrix(use,ncol=1)
+latlon <- coordinates(dem)
+latlon <- latlon[!is.na(values(dem)),]
 
-# may need to separate this into chunks to get it to run:
-#use1 <- use[1:3000000]
-#use1 <- use[3000001:6000000]
-#use1 <- use[6000001:length(use)]
-# if chunking not needed, then:
-use1 <- use
+chunksize = 5000000
+nc <- ceiling(length(use)/chunksize)
 
-# set up
-use1 <- matrix(use1,nrow=length(use1),ncol=1)
+# preallocate output
+hwtab <- data.frame(lon=numeric(),lat=numeric(),hw=numeric())
 
-# running 3million cells takes about 9.7 hours
-cl <- makeCluster(clnum)
-clusterExport(cl, c("calc_headwall","use1","buf","slope","slope_thres","demsmall2"))
-tic(paste0('calculate headwall metric'))
-headwall_metric_9mil <- parApply(cl,use1, 1, function(i) calc_headwall(iter=i,demsmall2=demsmall2,slope=slope,buf=buf,slope_thres=slope_thres))
-toc()
-stopCluster(cl)
+for (ll in 1:nc){
+  print(paste0('running group ',as.character(ll),' of ', as.character(nc)))
+  st = (ll-1)*chunksize+1
+  en = min(ll*chunksize, length(use))
+
+  use1 <- st:en
+  use1 <- matrix(use1,nrow=length(use1),ncol=1)
+
+  # running 3million cells takes about 9.7 hours
+  cl <- makeCluster(clnum)
+  clusterExport(cl, c("calc_headwall","use1","buf","slope","slope_thres","dem"))
+  tic(paste0('calculate headwall metric'))
+  headwall_metric <- parApply(cl,use1, 1, function(i) calc_headwall(iter=i,demsmall2=dem,slope=slope,buf=buf,slope_thres=slope_thres))
+  
+  # add to large output dataframe
+  hnew <- data.frame(latlon[st:en,],headwall_metric)
+  names(hnew) <- c('lon','lat','hw')
+  hwtab <- rbind(hwtab, hnew)
+  
+  #write.table(headwall_metric,paste0(outdir,'headwall/headwall_metric_group_',ll,'.csv'))
+  toc()
+  stopCluster(cl)
+
+}
+
+write.table(hwtab,paste0(outdir,'headwall_metric.csv'))
 
