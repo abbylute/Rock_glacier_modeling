168da9d3691cb8ae9117ee49ee2b5c1fa002e54b
abbylute <aby.lute@gmail.com>
2021-01-08 16:34:44 -0800

    output matfiles too, do not calculate headwall

--- get_slope_aspect_tpi.R
+++ get_slope_aspect_tpi.R
@@ -2,14 +2,14 @@
 
 library(raster)
 library(parallel)
+library(R.matlab)
 clnum <- detectCores()-2
 
 mdir <- '/Volumes/WDPassport/'
-mdir <- '/home/abby/'
 outdir <- paste0(mdir,'Rock_glacier_research/WUS/Data/Topography/')
-#dem <- raster('/Volumes/WDPassport/DATA/DEM/NED/210m/WUS_NED_210m.tif')
-dem <- raster(paste0(mdir,'DATA/Mapping/WUS_NED_210m.tif'))
-source(paste0(mdir,'Rock_glacier_research/WUS/Code/calc_headwall.R'))
+dem <- raster('/Volumes/WDPassport/DATA/DEM/NED/new/WUS_NED_210m.tif')
+
+#source(paste0(mdir,'Rock_glacier_research/WUS/Code/calc_headwall.R'))
 
 # create slope raster:
 terchar = terrain(dem, opt=c('slope','aspect','TPI'), unit='degrees', neighbors=8)
@@ -25,46 +25,58 @@ aspect <- terchar[[3]]
 writeRaster(aspect,paste0(outdir,'aspect_210m_WUS.tif')) 
 
 
+# Save slope, aspect, tpi as matfiles as well
+xy <- coordinates(dem)
+slope <- values(slope)
+aspect <- values(aspect)
+tpi <- values(tpi)
 
+writeMat(paste0(outdir,'slope_210m_WUS.mat'), 'xy'=xy,'slope'=slope)
+writeMat(paste0(outdir,'aspect_210m_WUS.mat'), 'xy'=xy,'aspect'=aspect)
+writeMat(paste0(outdir,'tpi_210m_WUS.mat'), 'xy'=xy,'tpi'=tpi)
+
+
+
+# headwall metric is better calculated in Matlab get_headwall.m. Way faster and don't have to chunk.
 # calculate headwall metric
-buf = 400 #radius in meters
-slope_thres = 30 # threshold slope in degrees
+#buf = 400 #radius in meters
+#slope_thres = 30 # threshold slope in degrees
 
-use <- which(!is.na(values(dem)))
-use <- matrix(use,ncol=1)
-latlon <- coordinates(dem)
-latlon <- latlon[!is.na(values(dem)),]
+#use <- which(!is.na(values(dem)))
+#use <- matrix(use,ncol=1)
+#latlon <- coordinates(dem)
+#latlon <- latlon[!is.na(values(dem)),]
 
-chunksize = 5000000
-nc <- ceiling(length(use)/chunksize)
+#chunksize = 5000000
+#nc <- ceiling(length(use)/chunksize)
 
 # preallocate output
-hwtab <- data.frame(lon=numeric(),lat=numeric(),hw=numeric())
+#hwtab <- data.frame(lon=numeric(),lat=numeric(),hw=numeric())
 
-for (ll in 1:nc){
-  print(paste0('running group ',as.character(ll),' of ', as.character(nc)))
-  st = (ll-1)*chunksize+1
-  en = min(ll*chunksize, length(use))
+#for (ll in 1:nc){
+#  print(paste0('running group ',as.character(ll),' of ', as.character(nc)))
+#  st = (ll-1)*chunksize+1
+#  en = min(ll*chunksize, length(use))
 
-  use1 <- st:en
-  use1 <- matrix(use1,nrow=length(use1),ncol=1)
+#  use1 <- st:en
+#  use1 <- matrix(use1,nrow=length(use1),ncol=1)
 
   # running 3million cells takes about 9.7 hours
-  cl <- makeCluster(clnum)
-  clusterExport(cl, c("calc_headwall","use1","buf","slope","slope_thres","dem"))
+#  cl <- makeCluster(clnum)
+#  clusterExport(cl, c("calc_headwall","use1","buf","slope","slope_thres","dem"))
   #tic(paste0('calculate headwall metric'))
-  headwall_metric <- parApply(cl,use1, 1, function(i) calc_headwall(iter=i,demsmall2=dem,slope=slope,buf=buf,slope_thres=slope_thres))
+#  headwall_metric <- parApply(cl,use1, 1, function(i) calc_headwall(iter=i,demsmall2=dem,slope=slope,buf=buf,slope_thres=slope_thres))
   
   # add to large output dataframe
-  hnew <- data.frame(latlon[st:en,],headwall_metric)
-  names(hnew) <- c('lon','lat','hw')
-  hwtab <- rbind(hwtab, hnew)
+#  hnew <- data.frame(latlon[st:en,],headwall_metric)
+#  names(hnew) <- c('lon','lat','hw')
+#  hwtab <- rbind(hwtab, hnew)
   
   #write.table(headwall_metric,paste0(outdir,'headwall/headwall_metric_group_',ll,'.csv'))
   #toc()
-  stopCluster(cl)
+#  stopCluster(cl)
 
-}
+#}
 
-write.table(hwtab,paste0(outdir,'headwall_metric.csv'))
+#write.table(hwtab,paste0(outdir,'headwall_metric.csv'))
 
