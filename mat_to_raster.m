73dba0bc5e4443e6238994565a611155019106a6
abbylute <aby.lute@gmail.com>
2021-03-25 19:39:42 -0700

    j

--- mat_to_raster.m
+++ mat_to_raster.m
@@ -1,7 +1,7 @@
 % for Rock Glacier Study, convert matfiles to rasters
 
 
-
+%% ON LAPTOP
 dir = '/Volumes/WDPassport/Rock_glacier_research/WUS/Data/';
 
 % headwall 5
@@ -24,4 +24,33 @@ R = georasterref('RasterSize',size(hw), ...
     'LatitudeLimits',[min(lat(:)) max(lat(:))],'LongitudeLimits',[min(lon(:)) max(lon(:))]);
 fn = [dir,'Topography/headwall_210m_WUS_3x3.tif'];
 geotiffwrite(fn, flipud(hw), R)
+
+
+
+%% ON THUNDER
+era = 'PRE';
+cdir = ['/home/abby/DATA/WRF/downscaled/WUS/',era,'/summaries/'];
+
+% lonlat
+ll = matfile('/home/abby/DATA/Mapping/WUS_NED_210m.mat');
+lon = ll.lon;
+lat = ll.lat;
+clear ll
+
+R = georasterref('RasterSize',size(lon), ...
+    'LatitudeLimits',[min(lat(:)) max(lat(:))],'LongitudeLimits',[min(lon(:)) max(lon(:))]);
+
+
+% temperatures
+tt = matfile([cdir,'tmean.mat']);
+tt = tt.tmean;
+tmin = min(tt,[],3);
+tmax = max(tt,[],3);
+tmean = mean(tt,3);
+
+R = georasterref('RasterSize',size(hw), ...
+    'LatitudeLimits',[min(lat(:)) max(lat(:))],'LongitudeLimits',[min(lon(:)) max(lon(:))]);
+fn = [dir,'Topography/headwall_210m_WUS_3x3.tif'];
+geotiffwrite(fn, flipud(hw), R)
+
 
